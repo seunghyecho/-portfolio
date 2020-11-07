448ee7ad4fa02855d945fc2f123789c56ecb7439
seunghyecho <tmdgp523@naver.com>
2020-11-07 17:50:43 +0900

    컨텐츠 내용/이미지 변경 및 코드 삭제

--- js/interactiveWeb copy.js
+++ js/interactiveWeb copy.js
@@ -1,10 +1,21 @@
 (() => {
+    // 5. 특정 이미지 활성화 되었을 떄 함수를 실행시킴
+    var actions={
+        birdFlies(key){ //true,false 구분 시 key 임의값 지정해서 조건문
+            document.querySelector('[data-index="2"] .bird').style.transform = key ? `translateX(${window.innerWidth}px)` : `translateX(-100%)`
+        },
+        birdFlies2(key){ //true,false 구분 시 key 임의값 지정해서 조건문
+            document.querySelector('[data-index="5"] .bird').style.transform = key ? `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)` : `translate(-100%)`
+        }
+    }
+
     var stepElems = document.querySelectorAll('.step');
     var graphicElems = document.querySelectorAll('.graphic-item');
     var currentItem = graphicElems[0]; //현재 활성화된 그래픽 아이템을 지정하고 있는변수 //0번 항상 활성화
     var ioIndex;
     var io = new IntersectionObserver((entries, observer) => {
         //요소의 가시성 관찰시키도록 등록-> 나타나거나 사라질 때 콜백함수가 실행
+        console.log(entries[0].target.dataset);
         ioIndex = entries[0].target.dataset.index * 1; //*1 은 숫자로 변경하기 위함
     });
 
@@ -16,11 +27,17 @@
     }
 
     // 3. 함수 기능 각각 따로 빼놓음
-    function activate() {
+    function activate(action) {
         currentItem.classList.add('visible');
+        if(action){
+            actions[action](true);
+        }
     }
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible');
+        if(action){
+            actions[action](false);
+        }
     }
 
     //2. 스크롤 할 경우
@@ -40,13 +57,9 @@
                 boundingRect.top < window.innerHeight * 0.8) {
                 inactivate();
                 currentItem = graphicElems[step.dataset.index]; //이미지 노출되어야 할  범위 설정
-                activate();
+                activate(currentItem.dataset.action);
             }
         }
-    })
-
-    window.addEventListener('load', () => {
-        setTimeout(() => scrollTo(0, 0), 100);
     })
 
     activate(); //첫 그래픽 이미지 항상 활성화
